a1852e74fe1bf35c5d161a34c5f098bb7b93e8fe
Cid-Jie <pcidjie@gmail.com>
2022-07-21 21:25:40 +0200

    Add rectification and loading fixtures

--- src/DataFixtures/DanceTeacherFixtures.php
+++ src/DataFixtures/DanceTeacherFixtures.php
@@ -57,7 +57,7 @@ class DanceTeacherFixtures extends Fixture
         'story' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         'dance_classes_id' => 'Danse orientale'
         ],
-        ['firstname' => 'Brooklyn',
+        ['firstname' => 'Edouard',
         'lastname' => 'Simmons',
         'photo' => 'https://i.pravatar.cc/306',
         'story' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
