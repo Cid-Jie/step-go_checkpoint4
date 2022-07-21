169fdf17b6b11b5b3e57da3b62da1814717adcea
Cid-Jie <pcidjie@gmail.com>
2022-07-21 08:34:12 +0200

    Add css

--- src/Controller/StepAndGoController.php
+++ src/Controller/StepAndGoController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StepAndGoController extends AbstractController
 {
-    #[Route('/step', name: 'app_step_and_go')]
+    #[Route('/', name: 'app_step_and_go')]
     public function index(): Response
     {
         return $this->render('step_and_go/index.html.twig');
