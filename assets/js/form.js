8f4595d3511b82542f4cd183073bbd744e2c9c1c
Cid-Jie <pcidjie@gmail.com>
2022-07-22 13:40:59 +0200

    Add scrollbar on planning

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,7 +12,12 @@ if (document.querySelector('.button-save-message')) {
         const buttonActionForm = document.querySelector('.button-save-message');
         if (inputEmailValue === "") {
             buttonActionForm.removeAttribute('disabled', 'disabled');
-            buttonActionForm.addEventListener('click', () => {
+            buttonActionForm.addEventListener('click', (e) => {
+                //e.preventDefault();
+                /* customBox.innerHTML = '<p>Merci pour votre message, nous vous répondrons très vite.</p>';
+                customBox.innerHTML += '<button id="modal-close">OK</button>';
+                modalShow();
+                return false; */
                 alert('Votre message a bien été envoyé, nous vous répondrons très vite.');
             });
 
@@ -20,6 +25,42 @@ if (document.querySelector('.button-save-message')) {
             buttonActionForm.setAttribute('disabled', 'disabled');
         }
     }
+
+/*     var modalContainer = document.createElement('div');
+    modalContainer.setAttribute('id', 'modal');
+
+    var customBox = document.createElement('div');
+    customBox.className = 'custom-box';
+
+    function modalShow() {
+        modalContainer.appendChild(customBox);
+        document.body.appendChild(modalContainer);
+
+        document.getElementById('modal-close').addEventListener('click', function() {
+            modalClose();
+        });
+
+        if (document.getElementById('modal-confirm')) {
+            document.getElementById('modal-confirm').addEventListener('click', function () {
+            console.log('Confirmé !');
+            modalClose();
+            });
+        } else if (document.getElementById('modal-submit')) {
+            document.getElementById('modal-submit').addEventListener('click', function () {
+                console.log(document.getElementById('modal-prompt').value);
+                modalClose();
+            });
+        }
+    }
+
+    function modalClose() {
+        while (modalContainer.hasChildNodes()) {
+            modalContainer.removeChild(modalContainer.firstChild);
+        }
+        document.body.removeChild(modalContainer);
+        document.location.href = "/";
+
+    }  */
 }
 
 
