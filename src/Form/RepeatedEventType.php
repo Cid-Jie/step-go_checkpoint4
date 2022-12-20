0c2357407e4290b95c4b3fab8ad5ff8312c4938f
Cid-Jie <pcidjie@gmail.com>
2022-12-20 22:37:58 +0100

    add readme

--- src/Form/RepeatedEventType.php
+++ src/Form/RepeatedEventType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\RepeatedEvent;
 use App\Service\NumberSelectService;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
