ac93f4d3fdaf999431b4a2efbb3e20013c4fd382
Cid-Jie <pcidjie@gmail.com>
2023-01-14 20:57:31 +0100

    add comment in api controller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -24,34 +24,37 @@ class ApiController extends AbstractController
     #[Route('/get-events', name: 'app_get_events')]
     public function index(EventRepository $eventRepository, Request $request): Response
     {
-        // On récupère le premier et le dernier jour de la semaine courante
+
+        // Je récupère le premier et le dernier jour de la semaine courante
         $firstDay = $request->query->get('start');
         $lastDay = $request->query->get('end');
 
-        // On transforme ces dates en nouvelle date avec une heure pour $lastDay qui se termine à 23:59:59
+        // Je transforme ces dates en nouvelle date avec une heure pour $lastDay qui se termine à 23:59:59
         $firstDay = new \DateTime($firstDay);
         $lastDay = new \DateTime($lastDay);
+        $firstDay->setTime(00,00,01);
         $lastDay->setTime(23,59,59);
 
-        // On récupére tous les événements et événements répétitifs en bdd compris dans la semaine
+        // Je récupére tous les événements (y compris les événements répétitifs) en bdd compris dans la semaine affichée
         $eventsFromDatabase = $eventRepository->findByWeek($firstDay, $lastDay);
 
-        /**
-         * On créé un nouveau tableau vide puis on boucle sur tous les événements pour extraire
-         * et modifier le comportement sur les événements répétitifs
-         */ 
+        // Je créé un nouveau tableau vide
+        // Je boucle sur tous les événements récupérés en bdd pour extraire
+        // et modifier le comportement sur les événements répétitifs
         $calculatedEvents = [];
         foreach ($eventsFromDatabase as $eventFromDatabase) {
             
+            // J'associe l'événement de la base de données à une nouvelle variable qui comprendra les informations modifiées
             $eventTransformed = $eventFromDatabase;
 
             // Si c'est un événement répétitif, on génère une occurence d'un événement répété
             if($eventFromDatabase instanceof RepeatedEvent) {
                 
-                // On récupère la date de départ et la date de fin de l'événement
+                // Je récupère la date de départ et la date de fin de l'événement auquel j'ajoute des horaires
                 $repetitionStart = $eventFromDatabase->getStart()->setTime(0, 0);
                 $repetitionEnd = $eventFromDatabase->getEnd()->setTime(23, 59);
 
+                // Je match le jour de la semaine de l'événement
                 $daysFromFirstday = match($eventFromDatabase->getDayOfWeek()) {
                     'monday' => 0,
                     'tuesday' => 1,
@@ -62,53 +65,55 @@ class ApiController extends AbstractController
                     'sunday' => 6
                 };
 
+                // J'instancie une nouvelle date pour connaître la prochaine itération de l'événement répétitif
+                // L'information d'un objet de la classe DateInterval est une instruction pour aller d'une date/moment à une autre date/moment
+                // Le date_add modifie un objet DateTime, avec le nombre de jours récupéré dans le match
+                // J'attribue la date de départ et le créneau horaire
                 $eventNextIterationDay = new \DateTime($request->query->get('start'));
                 $dateInterval = new DateInterval("P".$daysFromFirstday."D");
-             
                 $eventNextIterationDay = date_add($eventNextIterationDay, $dateInterval);
-                
-                // On vérifie si la prochaine itération peut exister entre la date de début et la date de fin
-             
                 $eventNextIterationStart = $eventNextIterationDay->setTime($eventFromDatabase->getStartHour(), $eventFromDatabase->getStartMinute());
+
+                // Je vérifie que la prochaine itération existe bien, si c'est le cas je crée un nouvel événement
                 $isValidIteration = $eventNextIterationStart > $repetitionStart && $eventNextIterationStart < $repetitionEnd;
                 
                 $eventTransformed = null;
-
                 if($isValidIteration) {
 
                     $eventTransformed = new Event();
                     
-                    // On extrait les caractéristiques communes aux événements
+                    // J'extrait les caractéristiques communes aux événements
                     $eventTransformed
                         ->setDanceClasses($eventFromDatabase->getDanceClasses())
                         ->setColor($eventFromDatabase->getColor())
                         ->setDescription($eventFromDatabase->getDescription());
                     
                     // La date de début est générée avant pour vérifier si l'événemnet rentre dans les bonnes conditions
-                    // donc on génére juste la date de fin à partir des informations et du créneau
+                    // donc je génére juste la date de fin à partir des informations et du créneau de la même manière
                     $eventNextIterationEnd = new \DateTime($request->query->get('start'));
                     $dateInterval = new DateInterval("P".$daysFromFirstday."D");
                     $eventNextIterationEnd = date_add($eventNextIterationEnd, $dateInterval);
                     $eventNextIterationEnd->setTime($eventFromDatabase->getEndHour(), $eventFromDatabase->getEndMinute());
 
-                    // On extrait les dates et créneau de l'événement
+                    // J'extrait les dates et créneau de l'événement
                     $eventTransformed
                         ->setStart($eventNextIterationStart)
                         ->setEnd($eventNextIterationEnd);
-
                 }
             }
             
-            // On push l'événement répétitif dans le nouveau tableau crée au début
+            // Je vérifie que mon événement répétitif n'est pas null et si c'est le cas, 
+            // je push l'événement répétitif dans le nouveau tableau crée au début
             if ($eventTransformed !== null) {
                 $calculatedEvents[] = $eventTransformed;
             }
         }
         
         $eventArray = [];
-
+        // Je boucle sur le nouveau tableau pour ré-attribuer chaque événement aux informations en base de données et envoyer tous les événements en base de données
         foreach ($calculatedEvents as $event) {
             
+            // dance classs peut être null
             $danceClassName = '';
             if($event->getDanceClasses()) {
                 $danceClassName = $event->getDanceClasses()->getName();
